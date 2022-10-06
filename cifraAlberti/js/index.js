853a6d3906145b33e660f2d0ec719c1c4d762c3c
AbnerHS <rick.santos123@hotmail.com>
2022-10-06 17:55:22 -0300

    feature: cifra de Alberti - método 2

--- cifraAlberti/js/index.js
+++ cifraAlberti/js/index.js
@@ -15,11 +15,11 @@ function girarDiscoMovel(discoAtual, chave, posFinal) {
   }
   //Girar para trás
   else {
-    var index = posChave
-    for (var i = index - 1; i < discoAtual.length; i++) {
+    var index = posChave - posFinal
+    for (var i = index; i < discoAtual.length; i++) {
       discoFinal += discoAtual[i]
     }
-    for (var i = 0; i < index - 1; i++) {
+    for (var i = 0; i < index; i++) {
       discoFinal += discoAtual[i]
     }
   }
@@ -68,6 +68,55 @@ function decifrarMetodo1(msgCifrada, chave) {
   return msgOriginal
 }
 
+function isNumber(char) {
+  return !Object.is(Number(char), NaN)
+}
+
+function cifrarMetodo2(msg, chave) {
+  var discoMovel = 'gklnprtvz&xysomqihfdbace'
+  var msgCifrada = msg[0]
+  var posChave = discoMovel.indexOf(chave)
+  var posLetra = discoFixo.indexOf(msg[0])
+  if (posChave != posLetra) {
+    discoMovel = girarDiscoMovel(discoMovel, chave, posLetra)
+  }
+  for (var i = 1; i < msg.length; i++) {
+    if (isNumber(msg[i])) {
+      msgCifrada += msg[i]
+      var posNum = discoFixo.indexOf(msg[i])
+      var novaChave = discoMovel[posNum]
+      discoMovel = girarDiscoMovel(discoMovel, novaChave, posLetra)
+    } else {
+      var posLetraDiscoMovel = discoFixo.toLowerCase().indexOf(msg[i])
+      if (posLetraDiscoMovel != -1)
+        msgCifrada += discoMovel[posLetraDiscoMovel]
+    }
+  }
+  return msgCifrada
+}
+
+function decifrarMetodo2(msgCifrada, chave) {
+  var discoMovel = 'gklnprtvz&xysomqihfdbace'
+  var msgOriginal = ""
+  var posChave = discoMovel.indexOf(chave)
+  var posLetra = discoFixo.indexOf(msgCifrada[0])
+  if (posChave != posLetra) {
+    discoMovel = girarDiscoMovel(discoMovel, chave, posLetra)
+  }
+  for (var i = 1; i < msgCifrada.length; i++) {
+    if (isNumber(msgCifrada[i])) {
+      var posNum = discoFixo.indexOf(msgCifrada[i])
+      var novaChave = discoMovel[posNum]
+      discoMovel = girarDiscoMovel(discoMovel, novaChave, posLetra)
+    } else {
+      var posLetraDiscoFixo = discoMovel.indexOf(msgCifrada[i])
+      if (posLetraDiscoFixo != -1)
+        msgOriginal += discoFixo[posLetraDiscoFixo]
+    }
+  }
+  return msgOriginal
+}
+
 var op, metodo;
 do {
   op = prompt("---------------CIFRA DE ALBERTI-------------\n1 - Cifrar\n2 - Decifrar\n0 - Sair")
@@ -82,6 +131,11 @@ do {
           var chave = prompt("Digite a chave do disco móvel")
           alert(cifrarMetodo1(fraseCifrar, chave.toLowerCase()))
           break
+        case '2':
+          var fraseCifrar = prompt("Insira a letra maiúscula inicial e os números na frase", fraseNormalizada)
+          var chave = prompt("Digite a chave do disco móvel")
+          alert(cifrarMetodo2(fraseCifrar, chave.toLowerCase()))
+          break
       }
       break
     case '2':
@@ -91,6 +145,10 @@ do {
         case '1':
           var chave = prompt("Digite a chave do disco móvel")
           alert(decifrarMetodo1(fraseCifrada, chave.toLowerCase()))
+          break
+        case '2':
+          var chave = prompt("Digite a chave do disco móvel")
+          alert(decifrarMetodo2(fraseCifrada, chave.toLowerCase()))
           break
       }
       break
